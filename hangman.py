6e5da8b3209e08054b45b7e45ddd68b39ae10a83
Lucas Stefanic <lucas.stefanic@zoho.com>
2021-12-21 13:12:15 -0600

    Part 2 complete: basic version of game working

---
+++ hangman.py
@@ -0,0 +1,41 @@
+def display_revealed_letters(SECRET_WORD,guessed_letters):
+
+    revealed = "Secret word: "
+    for i in range(len(SECRET_WORD)):
+        if ( SECRET_WORD[i] in guessed_letters ):
+            revealed += SECRET_WORD[i]
+        else:
+            revealed += "_"
+    print(revealed)
+
+def get_guess():
+
+    guess = "1"
+    while ( not guess.isalpha() ):
+        guess = input("Guess a letter or the entire word: ")
+    return guess.lower()
+
+def play():
+
+    SECRET_WORD = "cereal"
+    num_guesses = 7
+    guessed_letters = set()
+
+    while ( num_guesses > 0 ):
+        display_revealed_letters(SECRET_WORD,guessed_letters)
+        guess = get_guess()
+        if ( len(guess) > 1 ):
+            if ( guess == SECRET_WORD ):
+                print("You win!")
+                return
+            else:
+                num_guesses -= 1
+        else:
+            if ( SECRET_WORD.find(guess) != -1 ):
+                guessed_letters.add(guess)
+            else:
+                num_guesses -= 1
+
+    print("You lose")
+
+play()
